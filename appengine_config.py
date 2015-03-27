5a2caa27bf01cfecfe70777e313e20552a6f3b6c
Abdón Rodríguez Davila <a@abdonrd.com>
2015-03-27 02:11:35 +0100

    Update the appengine_config.py with the vendor api

--- appengine_config.py
+++ appengine_config.py
@@ -1,8 +1,6 @@
 """`appengine_config` gets loaded when starting a new application instance."""
 
-import sys
-import os.path
+from google.appengine.ext import vendor
 
-
-# Add `lib` subdirectory to `sys.path` to can load third-party packages.
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
+# Add any libraries installed in the "lib" folder.
+vendor.add('lib')
